79511c277268d656d86502682eabfde35b4a510b
GabrielSantoss <gabrazil03@hotmail.com>
2023-03-02 14:23:28 -0300

    exercico de veto inverso

--- src/estudos/exercicio/arrays/OrdemInversa.java
+++ src/estudos/exercicio/arrays/OrdemInversa.java
@@ -3,7 +3,23 @@ package estudos.exercicio.arrays;
 public class OrdemInversa {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		int [] vetor = {5, 8, 0, -5, 6, 2};
+		int count = 0;
+		
+		System.out.println("Posições do vetor original ");
+		while (count < (vetor.length)) {
+			
+			System.out.print(vetor[count] + " " );
+
+			count++;
+		}
+		
+		
+		System.out.println("\nPosições do vetor alterado ");
+		for(int i = (vetor.length -1); i >= 0; i-- ) {
+			System.out.print(vetor[i] + " " );
+		}
 
 	}
 
